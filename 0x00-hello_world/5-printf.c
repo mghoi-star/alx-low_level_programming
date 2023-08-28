77427037590b881af902f118a34c1fe5a63040a7
mghoi-star <terrymghoi@gmail.com>
2023-08-28 11:45:24 -0700

    hello,printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - always entry point
  *
- * Return: always (0)
+ * Return: always 0
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	Return(0);
+	return(0);
 }
