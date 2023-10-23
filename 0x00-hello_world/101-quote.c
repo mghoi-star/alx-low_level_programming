6e2213d7e8ff91eb3a58149966b8d1e55a1b6f66
mghoi-star <terrymghoi@gmail.com>
2023-10-23 06:16:41 -0700

    and that piece of art is useful

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  *  * main - Entry point
  *   *
  *    * Return: Always 0 (Success)
- *     */
+**/
 int main(void)
 {
 		char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
